1cd40e593704fbb5fe5f4824fc8be4cd86ec90ea
Elif <elffemre@gmail.com>
2022-07-06 22:01:28 +0300

    Initial commit

---
+++ RazorPizzeria/Pages/Forms/CustomPizza.cshtml.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using RazorPizzeria.Models;
+
+namespace RazorPizzeria.Pages.Forms
+{
+    public class CustomPizzaModel : PageModel
+    {
+        [BindProperty]
+        public PizzasModel Pizza { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
